54c97e28fdb2703d292527bd5d5db58dd7c5f557
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-05-14 15:14:09 +0300

    Initial Commit

---
+++ src/main/java/com/alpergayretoglu/online_student_election/OnlineStudentElectionApplication.java
@@ -0,0 +1,13 @@
+package com.alpergayretoglu.online_student_election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudentElectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStudentElectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpergayretoglu/online_student_election/OnlineStudentElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpergayretoglu.online_student_election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStudentElectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
