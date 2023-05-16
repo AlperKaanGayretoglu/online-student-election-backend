2bb36b44e8aaa0f09c914cef10cdff5897cc29ad
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-05-16 16:02:11 +0300

    Removed building attribute from Department

--- src/main/java/com/alpergayretoglu/online_student_election/model/entity/Department.java
+++ src/main/java/com/alpergayretoglu/online_student_election/model/entity/Department.java
@@ -14,11 +14,8 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String name;
-
-    @Column(nullable = false)
-    private String building;
 
     @ManyToOne
     private Faculty faculty;
