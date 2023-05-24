a1ee246a4e105443cb8380bfecd41f07e214270c
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-05-24 19:00:50 +0300

    Updated the TOKEN_ISSUER and TOKEN_AUDIENCE security constants

--- src/main/java/com/alpergayretoglu/online_student_election/security/SecurityConstants.java
+++ src/main/java/com/alpergayretoglu/online_student_election/security/SecurityConstants.java
@@ -6,8 +6,8 @@ public class SecurityConstants {
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String TOKEN_TYPE = "JWT";
-    public static final String TOKEN_ISSUER = "gguidesapi";
-    public static final String TOKEN_AUDIENCE = "gguidesapi";
+    public static final String TOKEN_ISSUER = "OnlineStudentElection";
+    public static final String TOKEN_AUDIENCE = "OnlineStudentElection";
 
     private SecurityConstants() {
     }
